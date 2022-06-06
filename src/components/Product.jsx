5579f2e5b672cc5ecda6992ec9c58cbf85d4c02c
emre15900 <79264023+emre15900@users.noreply.github.com>
2022-06-07 01:10:05 +0300

    props

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -4,7 +4,14 @@ function Product({product}) {
   return (
     <div>
         <div className="product">
+            <div className="countProduct">
+            <button>+</button> <b>0</b> <button>-</button>
+            </div>
             <img src={product.img} />
+            <div className="productsInfo">
+                <p>{product.name}</p>
+                <p>$ {product.price}</p>
+            </div>
         </div>
     </div>
   )
