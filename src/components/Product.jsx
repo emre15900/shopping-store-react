7859c3634671eedf52393f7c6f2837c7e46fc7b0
Emre Sayın <corabim.com1@gmail.com>
2022-06-13 00:04:35 +0300

    Update Product.jsx

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,61 +1,71 @@
-import React from 'react'
+import React from "react";
 
-function Product({product, basket, setBasket}) {
+export default function Product({ product, basket, setBasket }) {
+  const basketProduct = basket.find((item) => item.id === product.id);
 
   function addBasket() {
-    const addFind = basket.find(item => item.id === product.id)
+    const addFind = basket.find((item) => item.id === product.id);
     if (addFind) {
       addFind.amount += 1;
-      setBasket([...basket.filter(item => item.id !== product.id), {
-        id: product.id,
-        name: product.name,
-        price: product.price,
-        amount: addFind.amount,
-        img: product.img
-      }])
-    }
-    else {
-      setBasket([...basket,{
-        id: product.id,
-        name: product.name,
-        price: product.price,
-        amount: 1,
-        img: product.img
-      }])
+      setBasket([
+        ...basket.filter((item) => item.id !== product.id),
+        {
+          id: product.id,
+          name: product.name,
+          img: product.img,
+          price: product.price,
+          amount: addFind.amount,
+        },
+      ]);
+    } else {
+      setBasket([
+        ...basket,
+        {
+          id: product.id,
+          name: product.name,
+          price: product.price,
+          img: product.img,
+          amount: 1,
+        },
+      ]);
     }
   }
 
   function removeBasket() {
-    const removeFind = basket.find(item => item.id === product.id)
-    removeFind.amount -=1;
+    const removeFind = basket.find((item) => item.id === product.id);
+    removeFind.amount -= 1;
     if (removeFind.amount === 0) {
-      setBasket([...basket.filter(item => item.id !== product.id)])
-    }
-    else{
-      setBasket([...basket.filter(item => item.id !== product.id), {
-        id: product.id,
-        name: product.name,
-        price: product.price,
-        amount: removeFind.amount,
-        img: product.img
-      }])
+      setBasket([...basket.filter((item) => item.id !== product.id)]);
+    } else {
+      setBasket([
+        ...basket.filter((item) => item.id !== product.id),
+        {
+          id: product.id,
+          name: product.name,
+          img: product.img,
+          price: product.price,
+          amount: removeFind.amount,
+        },
+      ]);
     }
   }
 
   return (
     <div>
-        <div className="product">
-            <div className="countProduct">
-            <button onClick={addBasket}>+</button> <b>0</b> <button onClick={removeBasket}>-</button>
-            </div>
-            <img src={product.img} />
-            <div className="productsInfo">
-                <p>{product.name}</p>
-                <p>$ {product.price}</p>
-            </div>
+      <div className="product">
+        <div className="countProduct">
+          <button onClick={addBasket}>+</button>{" "}
+          <b> {(basketProduct && basketProduct.amount) || 0} </b>
+          <button disabled={!basketProduct} onClick={removeBasket}>
+            -
+          </button>
         </div>
+        <img src={product.img} />
+        <div className="productsInfo">
+          <p>{product.name}</p>
+          <p>$ {product.price}</p>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
-
-export default Product
