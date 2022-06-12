ae380b5268e99efda6dd759622eb9e29e4615c22
Emre Sayın <corabim.com1@gmail.com>
2022-06-13 01:12:55 +0300

    basket

--- src/App.js
+++ src/App.js
@@ -1,30 +1,39 @@
+import Basket from './components/Basket';
 import Header from './components/Header';
 import Product from './components/Product';
 import './index.css';
-import products from "./products.json"
-import {useState, useEffect} from 'react';
-function App() {
+import products from './products.json';
+import {useState,useEffect} from 'react';
+function App() {  
+  const [basket,setBasket] = useState([]);
+  const [cost,setCost] = useState("");
 
-  const [basket, setBasket] = useState([]);
+  useEffect( () => {
+    const totalPrice = basket.reduce((pre,basket) => pre +(basket.amount*basket.price),0);
+    setCost(totalPrice);
 
-  useEffect(()=>{
-    console.log(basket);
-  }, [basket])
+  },[basket])
 
   return (
     <>
-      <Header />
-      <div className="productContainer">
-        <div className="productBoxs">
-          {
-            products.map(product => {
-             return <Product key={product.id} basket={basket} setBasket={setBasket} product={product} />
-            })
-          }
-        </div>
-      </div>
+    <Header/>
+
+    <div className='productContainer'>
+    <div className='productBoxs'>
+
+    {
+      products.map(product => (
+        <Product key = { product.id} basket={basket} setBasket={setBasket}  product={product} />
+        ))
+    }
+
+    </div>
+    <Basket cost={cost} basket={basket}/>
+    </div>
+
     </>
-  );
+    
+    );
 }
 
 export default App;
